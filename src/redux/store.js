d38540d096a05c6687a762a18e2235ef34e38ac1
THAMS-14996 <thamaraikkannan.marimuthuvadivel@olympus-europa.com>
2020-07-19 01:16:05 +0530

    removing redux logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,7 +6,11 @@ import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [logger]; // Logger is a function passing into an array
+const middlewares = []; // Logger is a function passing into an array
+
+if(process.env.NODE_ENV === 'development'){
+  middlewares.push(logger);
+}
 
 export const store = createStore(
   rootReducer,
